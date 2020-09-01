0641a729c18e8385ff1958bee5dfb3abcdab1e9b
ameedfaridi <faridiameed5@gmail.com>
2020-09-01 14:07:44 +0530

    Updates

---
+++ precache-manifest.37188d377e86037c63cbededc7bdd7c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3e7c3208e292bb6c34c2d3d3217892",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "ab2b8060c4052db42d7e",
+    "url": "/Todo-app/static/css/main.b066aca5.chunk.css"
+  },
+  {
+    "revision": "0dc8b473503a8ab4c560",
+    "url": "/Todo-app/static/js/2.39b68b09.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-app/static/js/2.39b68b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2b8060c4052db42d7e",
+    "url": "/Todo-app/static/js/main.3eb4ed66.chunk.js"
+  },
+  {
+    "revision": "63db0ea377636940f26d",
+    "url": "/Todo-app/static/js/runtime-main.45f05433.js"
+  }
+]);
